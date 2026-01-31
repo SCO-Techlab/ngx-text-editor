84915ce8b09cc5ea65dae127ca7158d1246bcd81
scotechlab <sco.techlab@gmail.com>
2026-01-31 04:47:27 +0100

    init

--- projects/ngx-text-editor/src/text-editor/containers/text-editor.component.ts
+++ projects/ngx-text-editor/src/text-editor/containers/text-editor.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { IconComponent } from '../components';
+import { ICONS } from '../constants';
 
 @Component({
   selector: 'ngx-text-editor',
   standalone: true,
   templateUrl: './text-editor.component.html',
-  styleUrls: ['./text-editor.component.scss']
+  styleUrls: ['./text-editor.component.scss'],
+  imports: [
+    IconComponent
+  ]
 })
 export class TextEditorComponent implements OnInit {
+
+  public ICONS = ICONS;
 
   constructor() { }
 
