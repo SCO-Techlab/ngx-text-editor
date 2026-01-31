5bd48c3e65fd7be3e4b51ba6bc23625fdb71a216
scotechlab <sco.techlab@gmail.com>
2026-01-31 03:15:14 +0100

    init

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { TextEditorComponent } from 'projects/ngx-text-editor/src/components';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports: [TextEditorComponent]
 })
 export class HomeComponent implements OnInit {
 
