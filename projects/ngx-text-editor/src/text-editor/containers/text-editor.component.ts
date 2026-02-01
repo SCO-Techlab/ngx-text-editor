28019b4fc83a3dfa1cac0e45f021bfd86f045b47
scotechlab <sco.techlab@gmail.com>
2026-02-02 00:15:21 +0100

    text editor v1

--- projects/ngx-text-editor/src/text-editor/containers/text-editor.component.ts
+++ projects/ngx-text-editor/src/text-editor/containers/text-editor.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { IconComponent } from '../components';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ButtonComponent } from '../components';
 import { ICONS } from '../constants';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'ngx-text-editor',
@@ -8,16 +9,41 @@ import { ICONS } from '../constants';
   templateUrl: './text-editor.component.html',
   styleUrls: ['./text-editor.component.scss'],
   imports: [
-    IconComponent
+    ButtonComponent,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class TextEditorComponent implements OnInit {
 
+  @Output() change = new EventEmitter<string>();
+
   public ICONS = ICONS;
+  public textAreaForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.initTextAreaForm();
+    this.subscribeTextAreaChanges();
   }
 
+  private initTextAreaForm(): void {
+    this.textAreaForm = new FormGroup({
+      textArea: new FormControl('')
+    });
+  }
+
+  private subscribeTextAreaChanges(): void {
+    this.textAreaForm.valueChanges.subscribe(formControl => {
+      const value: string = formControl.textArea;
+      const formatedValue: string = this.formatTextAreaValue(value);
+      console.log(formatedValue);
+      this.change.emit(formatedValue);
+    });
+  }
+
+  private formatTextAreaValue(text: string): string {
+    return text;
+  }
 }
